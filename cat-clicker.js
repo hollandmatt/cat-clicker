f90a031ae74a76e375a7a93c3492d059a261cf14
Matt Holland <hollandmatt@gmail.com>
2015-07-15 16:39:53 -0700

    version 2

--- cat-clicker.js
+++ cat-clicker.js
@@ -1,10 +1,25 @@
-var clickCount = 0;
+var cats = [
+    {
+        name: 'Velvet',
+        image: 'https://farm9.staticflickr.com/8048/8094397080_f8e498ecc5_z.jpg',
+        count: 0
+    },
+    {
+        name: 'Inferior Cat',
+        image: 'https://www.petfinder.com/wp-content/uploads/2012/11/99059361-choose-cat-litter-632x475.jpg',
+        count: 0
+    }
+];
 
 $(document).ready(function() {
-    $('#click-count').text(clickCount);
+    cats.map(function (cat, index) {
+        $('#cat' + index + 'name').text(cat.name);
+        $('#cat' + index + 'count').text(cat.count);
+        $('#cat' + index + 'image').attr('src', cat.image);
 
-    $('#cat').click(function(e) {
-        clickCount++;
-        $('#click-count').text(clickCount);
+        $('#cat' + index + 'image').click(function() {
+            cat.count++;
+            $('#cat' + index + 'count').text(cat.count);
+        });
     });
 });
